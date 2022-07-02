e532ef010c22427a540e3970615d6e75366549aa
Joshua Nilsson <27570195+joshua-nilsson@users.noreply.github.com>
2022-07-02 14:03:15 -0600

    Firebase Init

--- src/lib/firebase.prod.js
+++ src/lib/firebase.prod.js
@@ -1,23 +1,19 @@
 import Firebase from 'firebase/app';
 import 'firebase/firestore';
 import 'firebase/auth';
-
-// 1) when seeding the database you'll have to uncomment this!
 // import { seedDatabase } from '../seed';
 
 const config = {
-  apiKey: '',
-  authDomain: '',
-  databaseURL: '',
-  projectId: '',
-  storageBucket: '',
-  messagingSenderId: '',
-  appId: '',
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_ID
 };
 
 const firebase = Firebase.initializeApp(config);
-// 2) when seeding the database you'll have to uncomment this!
+
 // seedDatabase(firebase);
-// 3) once you have populated the database (only run once!), re-comment this so you don't get duplicate data
 
 export { firebase };
